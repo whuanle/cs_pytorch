96883df71e6beccffde72a94e49d87b5b1cf9501
whuanle <1586052146@qq.com>
2024-12-15 21:16:08 +0800

    1

---
+++ maomi/Maomi.Torch/examples/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿using static TorchSharp.torchvision;
+using model = TorchSharp.torchvision.models;
+var resnet101 = model.resnet101();
+foreach (var item in resnet101.parameters())
+{
+    Console.WriteLine();
+}
